44453e9bef4c9f485f490c63e13ac408920db502
Nicolas Javier Hidalgo Cavieres <nicolas@MacBook-Pro-de-Nicolas.local>
2019-07-01 09:19:17 -0400

    js

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
     var select = document.getElementById('select');
     var options = [];
-    var titulos = document.querySelectorAll('h3');
-
-    for( var i = 0; titulos.length > i; i++){
-        var str = titulos[i].innerText.replace(/\s/g, '');
+    var titulos = document.querySelectorAll('h3'); 
+    for( var i = 0; titulos.length > i; i++ ){
+        var str = titulos[i].innerText.replace( /\s/g, '' );
         options.push();
         select.innerHTML += `
             <li> <a href="#${ str }">${ str }</a></li>
         `;
         titulos[i].id = str;  
-    }
-
+    } 
     select.addEventListener('change', function(value){
         window.location.href = this.value;
-    });
-
+    }); 
 });
